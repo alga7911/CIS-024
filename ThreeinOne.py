7d9f3e89113c5d446b3dfc43d18550ee496cf0f3
Alexander Gabaldon <112454514+alga7911@users.noreply.github.com>
2023-12-14 04:37:28 -0800

    Created all the games for this project

--- ThreeinOne.py
+++ ThreeinOne.py
@@ -9,7 +9,7 @@ def main_menu():
     print("q. Quit.")
 
 #Code for rock, paper, and scissors.
-
+    
     #this gets the players choice
 def get_rps_user_choice():
     rps_user_choice = input("Type your choice (rock, paper, or scissors)")
@@ -36,6 +36,7 @@ def rps_winner(rps_user_choice, rps_computer_choice):
     else:
         return("You lose.")
 
+#Displays the welcome message for Rock, Paper, Scissors.
 def rock_paper_scissors():
     print("Welcome to Rock, Paper, and Scissors!")
 
@@ -47,15 +48,104 @@ def rock_paper_scissors():
 
     result = rps_winner(rps_user_choice, rps_computer_choice)
     print(result)
-
-pass
+    pass         
 
 #Code for Heads or Tails
 def heads_or_tails():
+    #chooses either heads or tails
+    def coin_flip():
+        return random.choice(["heads", "tails"])
+    
+    #Gets the users guess
+    def HT_user_input():
+        user_input = input("Guess either heads or tails: ").lower()
+        while user_input not in ["heads", "tails"]:
+            #in the case they don't choose heads or tails
+            print("Invalid guess. Please choose heads or tails.")
+            user_input = input("Guess either heads or tails: ").lower()
+        return user_input
+    
+    #Welcome message for heads or tails game
+    def play_HT():
+        print("Welcome to Heads or Tails!")
+
+        user_input = HT_user_input()
+        coin_result = coin_flip()
+
+        print(f"The coin has landed on {coin_result}.")
+
+        #determines if the guess is correct
+        if user_input == coin_result:
+            print(f"You guessed {user_input}, which is right!")
+        else:
+            print(f"Sorry, you guessed {user_input}, which was wrong.")
+    play_HT()
     pass
 
 #Code for Guessing the Number
 def rng_guessing_game():
+    #This gives the user the ability to make the game easier or harder.
+    def get_upper_limit():
+        while True:
+            try:
+                upper_limit = int(input("Enter the limit for the game. (Higher the harder!)"))
+                if upper_limit > 0:
+                    return upper_limit
+                else:
+                    print("Choose a number thats greater than 0.")
+            except ValueError:
+             print("Invalid input. Try again!")
+
+    #This will get a random number between 1 and whatever the user picked
+    def rng_generation(upper_limit):
+        return random.randint(1, upper_limit)
+    #This is the part where the user guesses the number the code picked.
+    def get_user_number(upper_limit):
+        while True:
+            try:
+                user_number = int(input(f"Guess a number between 1 and {upper_limit}: "))
+                if 1 <= user_number <= upper_limit:
+                    return user_number
+                else:
+                    print(f"Enter a number between 1 and {upper_limit}: ")
+            except ValueError:
+                print("Invalid input. Please enter a valid number!")
+    #Displays the winning message or a hint for the player.
+    def rng_winner(secret_number, user_number):
+        if user_number == secret_number:
+            return "WOW! You guess the correct number!"
+        elif user_number < secret_number:
+            return "Too low, Try again!"
+        else:
+            return "Too high, Try again!"
+    
+    #Messaage when choosing 3
+    def play_rng():
+        print("Welcome to Guess the Number!")
+
+        upper_limit = get_upper_limit()
+        secret_number = rng_generation(upper_limit)
+
+        attempts = 0
+        max_attempts = 5 #This number can change to make it harder or easier.
+
+        #logic for calculate the amount of attempts a player has
+        while attempts < max_attempts:
+            user_number = get_user_number(upper_limit)
+            rng_result = rng_winner(secret_number, user_number)
+            print(rng_result)
+
+            #tells the player they won or lost and the amount of attempts it took.
+            if user_number == secret_number:
+                print(f"YOU GUESSES CORRECT! It took you {attempts + 1} attempts to get it!")
+                break
+
+            attempts += 1
+
+        if attempts == max_attempts:
+            print(f"Sorry, you didn't guess within {max_attempts} attempts. The correct answer was {secret_number}.")
+
+    play_rng()
     pass
 
 def main():
