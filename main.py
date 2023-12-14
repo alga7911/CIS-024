9e9baaf8981b1078d5308eb918572f9c17c32494
Alexander Gabaldon <112454514+alga7911@users.noreply.github.com>
2023-12-14 00:00:22 -0800

    Pasted Week 1 Assignments

---
+++ main.py
@@ -0,0 +1,16 @@
+# Put down your name
+my_name = "Alexander Gabaldon"
+# Put down the course name you're working on
+course_name = "Python Programming"
+# Current month (in integer please!)
+curr_month = 9 # change this to ..?
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now.")
+print("Hope you have a great time learing this course")
+
+today = 72
+yesterday = 76
+# print out the average temperuatre of today and yesterday
+# PEI: can you try to use `today` and `yesterday` variable to calculate the `avg_temp` here?
+avg_temp = 81 # fill in here!
+print(f"average temperature is {avg_temp}")
